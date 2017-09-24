5d9e75ae6413e7495eac161a722ad3ce545f2c1d
shimadama <ms-shi-md@spice.ocn.ne.jp>
2017-09-24 13:35:20 +0000

    change

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-require_relative 'application'
-
-require 'line_clinet'
-
 # Initialize the Rails application.
 Rails.application.initialize!
